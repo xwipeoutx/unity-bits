9ca7fb80cf471526d9f2dedff77127dfaab2b46d
Steve Leigh <steve.leigh@readify.net>
2019-02-13 14:37:40 +1000

    Adds behaviour example of tweening between 2 positions, controlled with an animator.

--- README.md
+++ README.md
@@ -8,19 +8,11 @@ This repository contains snippets and examples of helpful effects and scripts I'
 
 Simple effect to transition between alpha=0 and alpha=1 with a sweeping blend.  Good for revealing panels or terrain layers, for example.
 
-Shader can be found at `UnityHelpers/Assets/Shaders/Sweep/SweepReveal.shader`
-
-See `UnityHelpers/Assets/Shaders/Sweep/Example` for an example.
-
 ## Occlusion only
 
 Shader that writes to the depth buffer, but does not render any colour.  Useful in AR when you have a mesh of the "real world" and wish to ensure holograms "hide behind" them.
 
 Note the Render Queue is `Geometry-1` to ensure it renders first - or there isn't anything to occlude!
-
-Shader can be found at `UnityHelpers/Assets/Shaders/Occlusion/Occlusion Only.shader`
-
-See `UnityHelpers/Assets/Shaders/Occlusion/Example` for an example.
 
 ## Lighting
 
@@ -33,9 +25,11 @@ Includes
 - Blinn-phong (specular)
 - Fresnel (rim lighting)
 
-Shaders can be found at `UnityHelpers/Assets/Shaders/Lighting/`
+# Behaviours
 
-See `UnityHelpers/Assets/Shaders/Lighting/Example` for an example.
+## Transform Tween
+
+Tweens a game object between 2 other transforms.  Useful for moving panels around in space as interaction occurs.  Applies position, rotation and scale
 
 # Gizmos
 
---
+++ UnityHelpers/Assets/Behaviours/TransformTween/TransformTween.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class TransformTween : MonoBehaviour
+{
+    public Transform pointA;
+    public Transform pointB;
+    public Transform target;
+
+    [Range(0, 1)] public float t;
+
+    void Update()
+    {
+        if (target == null || pointA == null || pointB == null)
+            return;
+
+        var time = t;
+        target.localPosition = Position(time);
+        target.localRotation = Rotation(time);
+        target.localScale = Scale(time);
+    }
+
+    private Quaternion Rotation(float time)
+    {
+        return Quaternion.Lerp(pointA.localRotation, pointB.localRotation, time);
+    }
+
+    private Vector3 Position(float time)
+    {
+        return Vector3.Lerp(pointA.localPosition, pointB.localPosition, time);
+    }
+
+    private Vector3 Scale(float time)
+    {
+        return Vector3.Lerp(pointA.localScale, pointB.localScale, time);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        var mesh = target.GetComponentInChildren<MeshFilter>()?.sharedMesh;
+        
+        if (target == null || pointA == null || pointB == null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawCube(transform.position, Vector3.one * 0.2f);
+            return;
+        }
+
+        var numSteps = 20f;
+        for (var i = 0; i < numSteps; i++)
+        {
+            var t = i / numSteps;
+            DrawSnapshot(t, mesh);
+        }
+    }
+
+    private void DrawSnapshot(float time, Mesh mesh)
+    {
+        Gizmos.color = Color.Lerp(Color.black, Color.white, time);
+        var localTransform = Matrix4x4.TRS(Position(time), Rotation(time), Scale(time));
+        Gizmos.matrix = transform.localToWorldMatrix * localTransform;
+
+        if (mesh != null)
+            Gizmos.DrawWireMesh(mesh);
+        else
+            Gizmos.DrawFrustum(Vector3.zero, 50, 1, 0.1f, 1);
+    }
+}
